4114d21a2c421881ec151336979f69682463ea84
niamhtalbot11 <74904383+niamhtalbot11@users.noreply.github.com>
2024-02-29 19:00:32 +0700

    basics

--- src/index.js
+++ src/index.js
@@ -1,35 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "./App.css";
+import App from "./App";
+import SearchEngine from "./SearchEngine";
 
-
-const rootElement = document.getElementById("#root");
+const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
     <div className="App">
-      <h1>React Events</h1>
-
-      <h3>Challenge 1</h3>
-      <p>
-        Create a new component called Weather which renders a "Current
-        Temperature" button. Add the button here.
-      </p>
-    
-      <h3>Challenge 2</h3>
-      <p>
-        When the button is clicked, alert "Coming soon..." Add the button here.
-      </p>
-      
-      <h3>Challenge 3</h3>
-      <p>
-        Create a new component called Forecast which renders a link "See
-        forecast". When clicking on it, alert "Forecast feature is not ready
-        yet". Include the component here.
-      </p>
-      
+      <h1>Weather App</h1>
+      <SearchEngine />
+      <br />
+      <div class="temperature"></div>
+      <div class="description"></div>
+      <div class="humidity"></div>
+      <div class="windSpeed"></div>
+      <img src="" alt="" id="icon" />
     </div>
+    <App />
   </StrictMode>
 );
