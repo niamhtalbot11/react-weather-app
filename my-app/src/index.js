804d75dcdc08adae299f03d1eb5c0a3565aa7b30
niamhtalbot11 <74904383+niamhtalbot11@users.noreply.github.com>
2024-03-01 10:21:35 +0700

    install react

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App";
-import SearchEngine from "./SearchEngine";
+import App from "./app";
+import SearchEngine from "./searchEngine";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
